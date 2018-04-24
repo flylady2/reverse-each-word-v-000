29731985f10593f3ba06442a6bb0a8d44d181049
Leslie Stevens <flylady@mail.utexas.edu>
2018-04-24 21:24:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,9 +5,3 @@ def reverse_each_word(string)
   end
   return new_array.join(" ")
 end
- def reverse_each_word(string)
-   array = string.split(" ")
-   array.collect do|item|
-     item.reverse!
-   end
- end
